215ccab138072152ca2a8fab203ff24fa294597d
learningdevsecops1 <learningdevsecops1@gmail.com>
2025-01-01 23:54:02 -0600

    s

--- Test2.sh
+++ Test2.sh
@@ -8,14 +8,13 @@ then
     echo "Usage: $0 <package>"
     exit 1
 else 
-    for (( i=1; i<=varcount; i++ ))
+    for i in "$@"
     do
-        echo "Installing $1"
-    if [ $USERID -ne 0 ]
+        if [ $USERID -ne 0 ]
         then
             echo "User is not root , running the script with sudo"
             sudo su -c "cd /home/ec2-user/shell-script/ && dnf install $i -y"
-        if [ $? -ne 0 ]
+            if [ $? -ne 0 ]
             then
                 echo "Installing $i ... FAILURE"
                 exit 1
@@ -26,5 +25,7 @@ else
         else
             echo "User is root, skipping $i installation"
 fi
-exit 
+exit 0
+done
+fi
 
