ebc95cf707b3b9051bfe5f9441863666da1f18f1
learningdevsecops1 <learningdevsecops1@gmail.com>
2025-01-01 23:09:47 -0600

    Improve MySQL installation script by consolidating commands and enhancing user feedback

--- Test1.sh
+++ Test1.sh
@@ -4,17 +4,17 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "User is not root , running the scrrpt with sudo"
-    sudo su -c "cd /home/ec2-user/shell-script/"
-    dnf install mysql -y
+    echo "User is not root , running the script with sudo"
+    sudo su -c "cd /home/ec2-user/shell-script/ && dnf install mysql -y"
     if [ $? -ne 0 ]
     then
         echo "Installing MySQL ... FAILURE"
         exit 1
     else
         echo "Installing MySQL ... SUCCESS"
-    cd /home/ec2-user/shell-script/
+        cd /home/ec2-user/shell-script/
+    fi
 else
-    echo "MySQL is already ... INSTALLED"    
+    echo "User is root, skipping MySQL installation"
 fi
 exit 0
